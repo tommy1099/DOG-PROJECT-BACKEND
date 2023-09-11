29871844581ac2b13b71dec11d48adc3bbaa587a
tommy <46693705+Enigmatic1099@users.noreply.github.com>
2023-09-11 14:49:09 +0330

    asd

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -11,7 +12,6 @@ try {
 } catch (error) {
   console.log(error);
 }
-
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("Connected to DB"));
@@ -22,15 +22,20 @@ app.listen(10000, `0.0.0.0`, () => {
 
 app.use(cors({ methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"] }));
 
-app.get("/", cors(), async (req, res) => {
+const fetchFunc = async () => {
   await fetch("https://dog.ceo/api/breeds/image/random")
     .then((response) => response.json())
-    .then((data) => (res.body = data.message))
+    .then((data) => {
+      return data.message;
+    })
     .catch((err) => console.log(err));
+};
 
+app.get("/", cors(), async (req, res) => {
+  let fetchedDog = await fetchFunc;
   try {
-    const dog_db_model = await new DOGDB({
-      src: res.body,
+    const dog_db_model = new DOGDB({
+      src: fetchedDog,
     });
     const newDog = await dog_db_model.save();
     res.status(201).send(newDog.src);
